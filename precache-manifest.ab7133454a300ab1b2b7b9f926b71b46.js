0b01614619b11c736f18d2682a0702987351660f
Felipe Villarinho <felipe.villarinho@gmail.com>
2019-08-07 20:36:14 -0300

    Updates

---
+++ precache-manifest.ab7133454a300ab1b2b7b9f926b71b46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18096ef7a94773492adeb9326437c9f7",
+    "url": "/countriesapp/index.html"
+  },
+  {
+    "revision": "caf5b93f9b530d8900ee",
+    "url": "/countriesapp/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "bbe69a96ca0e254881b7",
+    "url": "/countriesapp/static/css/main.b9b0ddc6.chunk.css"
+  },
+  {
+    "revision": "caf5b93f9b530d8900ee",
+    "url": "/countriesapp/static/js/2.bb8397d8.chunk.js"
+  },
+  {
+    "revision": "bbe69a96ca0e254881b7",
+    "url": "/countriesapp/static/js/main.0890ee5e.chunk.js"
+  },
+  {
+    "revision": "2dbaac6280f0d37345c2",
+    "url": "/countriesapp/static/js/runtime~main.3b104c4a.js"
+  }
+]);
